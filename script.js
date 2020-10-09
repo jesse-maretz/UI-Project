daaab36a451654b3455f5c296aa59df23bf46bc0
Jesse Maretz <jesse.maretz@gmail.com>
2020-10-09 10:00:21 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+let url = 'https://api.nasa.gov/planetary/apod'
+//let headers = {headers: {'hjLU1COK5d19vjU34PL1XQFzDEJ6bswts34PHU5M'}}
+let apiKey = 'hjLU1COK5d19vjU34PL1XQFzDEJ6bswts34PHU5M'
+let urlWithApi = url + '?api_key=' + apiKey
