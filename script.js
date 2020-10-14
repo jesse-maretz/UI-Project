59ae12b21f843642a49d4baccfcd4fa8aa6f0390
Jesse Maretz <jesse.maretz@gmail.com>
2020-10-14 00:43:55 -0400

    initial commit

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let url = 'https://api.nasa.gov/planetary/apod'
 //Get my personl API key
 let apiKey = 'hjLU1COK5d19vjU34PL1XQFzDEJ6bswts34PHU5M'
 //Concat the url with my API key to create one var
-
 //Get left button
 let lBtn = document.querySelector('#leftArrow')
 //Get right button
@@ -12,27 +11,44 @@ let rBtn = document.querySelector('#rightArrow')
 //Get the astronomy picture of the day
 let apod = document.querySelector('.apodImg')
 let dateCounter = 9
-let date = `2020-10-0${dateCounter}`
-
-let urlWithApi = url + '?api_key=' + apiKey + `&date=${date}`
+function minusD() {
+    newDate = dateCounter - 1
+    urlWithApi = `${url}?api_key=hjLU1COK5d19vjU34PL1XQFzDEJ6bswts34PHU5M&date=2020-10-0${newDate}`
+    console.log(typeof(newDate))
+    dateCounter = newDate
+    return newDate
+}
+let date = '2020-10-0' + dateCounter
+//let urlWithApi = url + '?api_key=' + apiKey + `&date=${date}`
+let urlWithApi = `${url}?api_key=hjLU1COK5d19vjU34PL1XQFzDEJ6bswts34PHU5M&date=2020-10-0${dateCounter}`
 //Add click event listener to left arrow button
     //On click, go to the previous day's APOD
-
-    let urlAllTogether = 
-lBtn.addEventListener('click', function(e){
-    e.preventDefault()
+lBtn.addEventListener('click', leftBtn
+    /*e.preventDefault()
     fetch(urlWithApi)
     .then(res=>res.json())
-    .then(data => apod.src = data.url)
-    dateCounter = dateCounter--;
-})
-
+    .then(data => {
+        console.log(urlWithApi)
+        apod.src = data.url
+        --dateCounter;
+    } )*/
+)
+function leftBtn() {
+    fetch(urlWithApi)
+        .then(res=>res.json())
+        .then(data => {
+            console.log(urlWithApi)
+            apod.src = data.url
+        })
+        minusD()
+}
 //Add click event listener to left arrow button
     //On click, go to the next day's APOD
 rBtn.addEventListener('click', function(e){
     e.preventDefault()
-    fetch(urlWithApi, headers)
+    fetch(urlWithApi)
     .then(res=>res.json())
     .then(data => apod.src = data.url)
 })
 
+//https://api.nasa.gov/planetary/apod?api_key=hjLU1COK5d19vjU34PL1XQFzDEJ6bswts34PHU5M&date=2020-10-09
