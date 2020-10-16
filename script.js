fdf050167fc80335fce5ec143e03117c730e44a8
Jesse Maretz <jesse.maretz@gmail.com>
2020-10-15 22:04:11 -0400

    finally got the slide to work

--- script.js
+++ script.js
@@ -18,21 +18,19 @@ function minusD() {
     dateCounter = newDate
     return newDate
 }
+function plusD() {
+    newDate = dateCounter + 1
+    urlWithApi = `${url}?api_key=hjLU1COK5d19vjU34PL1XQFzDEJ6bswts34PHU5M&date=2020-10-0${newDate}`
+    console.log(typeof(newDate))
+    dateCounter = newDate
+    return newDate
+}
 let date = '2020-10-0' + dateCounter
 //let urlWithApi = url + '?api_key=' + apiKey + `&date=${date}`
 let urlWithApi = `${url}?api_key=hjLU1COK5d19vjU34PL1XQFzDEJ6bswts34PHU5M&date=2020-10-0${dateCounter}`
 //Add click event listener to left arrow button
     //On click, go to the previous day's APOD
-lBtn.addEventListener('click', leftBtn
-    /*e.preventDefault()
-    fetch(urlWithApi)
-    .then(res=>res.json())
-    .then(data => {
-        console.log(urlWithApi)
-        apod.src = data.url
-        --dateCounter;
-    } )*/
-)
+lBtn.addEventListener('click', leftBtn)
 function leftBtn() {
     fetch(urlWithApi)
         .then(res=>res.json())
@@ -42,13 +40,13 @@ function leftBtn() {
         })
         minusD()
 }
-//Add click event listener to left arrow button
-    //On click, go to the next day's APOD
-rBtn.addEventListener('click', function(e){
-    e.preventDefault()
+rBtn.addEventListener('click', rightBtn)
+function rightBtn() {
     fetch(urlWithApi)
-    .then(res=>res.json())
-    .then(data => apod.src = data.url)
-})
-
-//https://api.nasa.gov/planetary/apod?api_key=hjLU1COK5d19vjU34PL1XQFzDEJ6bswts34PHU5M&date=2020-10-09
+        .then(res=>res.json())
+        .then(data => {
+            console.log(urlWithApi)
+            apod.src = data.url
+    })
+    plusD()
+}
